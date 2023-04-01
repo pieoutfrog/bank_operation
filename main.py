0acef05c2cdfc39d08e122b88bb9f58181b63028
Alice <despero45@gmail.com>
2023-04-02 00:40:17 +1000

    Если бы мы знали, что это такое, но мы не знаем

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ executed_data = utils.get_state(all_data)
 
 sorted_operations = sorted(executed_data, key=lambda i: i['date'], reverse=True)
 first_five = sorted_operations[0:5]
-print(first_five)
-
-for i in first_five:
-    date = i.get('date')
-    formated_date = date.replace('-', '.')
-    print(f"""{formated_date[8:10]}.{formated_date[5:7]}.{formated_date[0:4]} {i.get('description')}
-{i.get('from')} -> {i.get('to')}""", '\n')
 
 
-# for values in first_five.items():
-#     print(values)
+def all_data(first_five):
+    for i in first_five:
+        sum_of_operation = (i['operationAmount']['amount'])
+        currency_of_operation = (i['operationAmount']['currency']['name'])
+        date = i.get('date')
+        sender = str([i.get('from')]).replace('None', 'Anonymous')
+        sender = sender.replace('[', '')
+        sender = sender.replace("]", '')
+        sender = sender.replace("'", '')
+        formated_date = date.replace('-', '.')
+        for val in sender:
+            if val is int:
+
+        if sender is None:
+            sender.replace('Отправитель неизвестен')
+        descripton = str(i.get('description')).replace("'", "")
+        print(f"""{formated_date[8:10]}.{formated_date[5:7]}.{formated_date[0:4]} {i.get('description')}
+{sender} -> {i.get('to')}
+{sum_of_operation} {currency_of_operation}""", '\n')
+
+
+all_data(first_five)
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import json
 from datetime import datetime
 
+
 def load_data(raw_data):
     with open("operations.json") as f:
         raw_data = json.load(f)
         return raw_data
+
 
 def get_state(dates):
     executed_operations = []
@@ -14,5 +16,4 @@ def get_state(dates):
                 executed_operations.append(state)
     return executed_operations
 
-
-#def printed_operations():
+# def printed_operations():
